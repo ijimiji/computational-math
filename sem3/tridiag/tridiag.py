129e387ff980b603af5e8aac77f2eeeccb1ba99b
Jahor Laryn <mrojahor@gmail.com>
2021-10-07 20:19:09 +0300

    Add code comments.

--- sem3/tridiag/tridiag.py
+++ sem3/tridiag/tridiag.py
@@ -1,5 +1,8 @@
 from random import randint
 
+N = 9
+k = 1
+
 
 def find_error(x):
     y = [i + 1.0 for i in range(len(x))]
@@ -8,10 +11,9 @@ def find_error(x):
     return error / max_y
 
 
-N = 2
-k = 1
 r = lambda: randint(-10, 10) * 1.00
 
+# Задаем вектора, которые стоят на главной диагонали, под и над ней
 a = [0.0] + [r() for _ in range(1, N + 1)]
 b = [r() for _ in range(0, N)]
 c = (
@@ -19,19 +21,21 @@ c = (
     + [abs(a[i]) + abs(b[i]) + 1.5 * k for i in range(1, N)]
     + [abs(a[N]) + 1.5 * k]
 )
+# Задаем точное решение и резервируем ресурсы для численного решения
 y = [i + 1.0 for i in range(N + 1)]
-f = [0.0] * (N + 1)
 x = [0.0] * (N + 1)
 
+# Задаем вектор правой части
+f = [0.0] * (N + 1)
 f = (
     [c[0] * y[0] + -b[0] * y[1]]
     + [-a[i] * y[i - 1] + c[i] * y[i] + -b[i] * y[i + 1] for i in range(1, N)]
     + [-a[N] * y[N - 1] + c[N] * y[N]]
 )
 
+# Прямой ход
 alpha = [0.0]
 beta = [0.0]
-
 alpha.append(b[0] / c[0])
 beta.append(f[0] / c[0])
 for i in range(1, N):
@@ -40,10 +44,12 @@ for i in range(1, N):
     beta.append((f[i] + a[i] * beta[i]) / product)
 beta.append((f[N] + a[N] * beta[N]) / (c[N] - a[N] * alpha[N]))
 
+# Обратный ход
 x[N] = beta[N + 1]
 for i in reversed(range(N)):
     x[i] = alpha[i + 1] * x[i + 1] + beta[i + 1]
 
+# Выводим результаты
 print(f"a ={a}")
 print(f"b ={b}")
 print(f"c ={c}")
@@ -51,4 +57,4 @@ print(f"f ={f}")
 print(f"y*={x}")
 print(f"y ={y}")
 
-print(find_error(x))
+print(f"Погрешность {find_error(x)}")
