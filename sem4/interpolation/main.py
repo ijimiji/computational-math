0262cf5d4e49c698cfb218e4f1294ec18ab3b40c
Jahor Laryn <mrojahor@gmail.com>
2022-03-23 14:14:38 +0300

    Fix report.

--- sem4/interpolation/main.py
+++ sem4/interpolation/main.py
@@ -7,8 +7,8 @@ b =  2
 f_1 = lambda x: exp(cos(x))
 f_2 = lambda x: abs(x * abs(x) - 1)
 
-x_ch = lambda k, n: (a+b)/2  + (b-a) / 2 * cos((2*k + 1)*pi / (2*n +2))
-x_ = lambda i, n: a + i*(b-a)/n
+x_ch = lambda k, n: (a+b)/2  + (b-a) / 2 * cos((2*k + 1)*pi / (2*(n-1) +2))
+x_ = lambda i, n: a + i*(b-a)/(n-1)
 
 def newton(x, y, n):
     xss = [x(i, n) for i in range(n)]
@@ -36,7 +36,7 @@ def compare(f, n):
     c = newton(x_, f, n)
     c_ch = newton(x_ch, f, n)
     m = 100
-    xss = [x_(i, m) for i in range(m)]
+    xss = [x_(i, m) for i in range(m+1)]
     print(n)
     print(f"Ordinary error: {max(f, c)}")
     print(f"Chebyshev error: {max(f, c_ch)}")
@@ -61,10 +61,10 @@ def max(f, c):
     return m
 
 print("f_1")
-for i in [5, 10, 15, 20]:
+for i in [6, 11, 16, 21]:
     compare(f_1, i)
 
 print("f_2")
-for i in [5, 10, 15, 20]:
+for i in [6, 11, 16, 21]:
     compare(f_2, i)
     
